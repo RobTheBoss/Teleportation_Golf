2ac60264c469a5ab7caf92ce24243f37189343f1
RobTheBoss <Robertbato825@gmail.com>
2024-04-06 14:38:30 -0400

    Working portals

--- Teleport_Golf/Assets/Scripts/Portal.cs
+++ Teleport_Golf/Assets/Scripts/Portal.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(CircleCollider2D))]
 public class Portal : MonoBehaviour
 {
+    public Transform otherPortal;
+    static float cooldownTotal = 0.1f;
+    static float cooldown = 0.0f;
+    static bool canTeleport = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,23 @@ public class Portal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (cooldown > 0)
+        {
+            canTeleport = false;
+            cooldown -= Time.unscaledDeltaTime;
+        }
+        else
+        {
+            canTeleport = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Golfball") && canTeleport)
+        {
+            collision.transform.position = otherPortal.position;
+            cooldown = cooldownTotal;
+        }
     }
 }
